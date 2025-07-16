c24b2b3e1422588ba9ce1c8d759d8468d87110df
sharath <anzaserviesllp.com>
2025-07-16 11:41:06 +0530

    added the test cases for the weather controller

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -7,16 +7,16 @@ class WeatherController < ApplicationController
   end
 
   # this method will call the weather api
-  def fetch_weather
+  
 
+  def fetch_weather
     city = params[:city]
     api_key = ENV['WEATHER_API_KEY']
     base_url = ENV['WEATHER_API_URL']
     url = "#{base_url}?q=#{city}&units=metric&appid=#{api_key}"
     response = HTTParty.get(url)
-    
-    @weather = response.parsed_response
-    respond_to do |format|
+ @weather = JSON.parse(response.body)
+     respond_to do |format|
       format.js
     end
   end
--- test/controllers/weather_controller_test.rb
+++ test/controllers/weather_controller_test.rb
@@ -1,31 +1,31 @@
 require 'test_helper'
 
 class WeatherControllerTest < ActionDispatch::IntegrationTest
-  setup do
+  def setup
     @city = "Bangalore"
-    @api_key = ENV['WEATHER_API_KEY']
-    @base_url = ENV['WEATHER_API_URL']
+    @api_key = ENV["WEATHER_API_KEY"]
+    @base_url = ENV["WEATHER_API_URL"]
     @url = "#{@base_url}?q=#{@city}&units=metric&appid=#{@api_key}"
 
+    # Mock weather API response
     @mock_response = {
       "weather" => [{ "description" => "clear sky", "icon" => "01d" }],
       "main" => { "temp" => 28.0 },
       "name" => "Bangalore"
     }.to_json
 
-    # WebMock stub for GET request
     stub_request(:get, @url)
       .to_return(status: 200, body: @mock_response, headers: { 'Content-Type' => 'application/json' })
   end
 
-  test "should fetch weather via JS with GET request" do
+  test "should get weather data and render js" do
     get fetch_weather_url, params: { city: @city }, xhr: true
 
     assert_response :success
-    assert_includes @response.body, "Bangalore" if @response.body.present?
+    assert_match "Bangalore", @response.body if @response.body.present?
   end
 
-  test "should get index page" do
+  test "should load index page" do
     get weather_index_url
     assert_response :success
   end
